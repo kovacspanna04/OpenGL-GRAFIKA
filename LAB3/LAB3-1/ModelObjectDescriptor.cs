2094d81cb6ddaf883e013ec209c678d88bc1e301
kovacspanna04 <kovacspanna3@gmail.com>
2025-04-16 20:42:48 +0300

    lab3-1 kesz

---
+++ LAB3/LAB3-1/ModelObjectDescriptor.cs
@@ -0,0 +1,147 @@
+﻿using Silk.NET.OpenGL;
+using Silk.NET.Vulkan;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrafikaSzeminarium
+{
+    internal class ModelObjectDescriptor : IDisposable
+    {
+        private bool disposedValue;
+
+        public uint Vao { get; private set; }
+        public uint Vertices { get; private set; }
+        public uint Colors { get; private set; }
+        public uint Indices { get; private set; }
+        public uint IndexArrayLength { get; private set; }
+
+        private GL Gl;
+
+        public float RectangleWidth { get; } = 1.0f;        // a teglalap szelessege mindig 1
+
+        private static float nx = MathF.Sin(10.0f * MathF.PI / 180.0f);         // 10 fok szinusza
+        private static float nz = MathF.Cos(10.0f * MathF.PI / 180.0f);     // 10 fok koszinusza
+
+        // mind a 4 ponthoz az x,y,z koordinatak + a harom normalvektor komponensei
+        private static float[] flatRectang = new float[] {
+                -0.5f, 0f, 0f, 0f, 0f, 1f,      // a normalvektor a Z tengely pozitiv iranyaba mutat
+                0.5f, 0f, 0f, 0f, 0f, 1f,
+                0.5f, 4.0f, 0f, 0f, 0f, 1f,
+                -0.5f, 4.0f, 0f, 0f, 0f, 1f
+            };
+        private static float[] roundRectang = new float[] {
+                -0.5f, 0f, 0f, -nx, 0f, nz,     // a poziciok ugyanazok, csak a normalvektort valtoztattam(-10 fokkal megdontott teglalap)
+                0.5f, 0f, 0f, nx, 0f, nz,
+                0.5f, 4.0f, 0f, nx, 0f, nz,
+                -0.5f, 4.0f, 0f, -nx, 0f, nz
+            };
+
+        private static float[] flatRectangColor = new float[] {
+                0.0f, 1.0f, 0.0f, 1.0f,
+                0.0f, 1.0f, 0.0f, 1.0f,
+                0.0f, 1.0f, 0.0f, 1.0f,
+                0.0f, 1.0f, 0.0f, 1.0f
+            };
+        private static float[] roundRectangColor = new float[] {
+                1.0f, 0.0f, 0.0f, 1.0f,
+                1.0f, 0.0f, 0.0f, 1.0f,
+                1.0f, 0.0f, 0.0f, 1.0f,
+                1.0f, 0.0f, 0.0f, 1.0f
+            };
+
+        public unsafe static ModelObjectDescriptor CreateRectangle(GL Gl, bool round)
+        {
+            uint vao = Gl.GenVertexArray();
+            Gl.BindVertexArray(vao);
+
+            float[] vertexArray;
+            float[] colorArray;
+
+            if (round)
+            {
+                vertexArray = roundRectang;
+                colorArray = roundRectangColor;
+            }
+            else
+            {
+                vertexArray = flatRectang;
+                colorArray = flatRectangColor;
+            }
+
+            uint[] indexArray = new uint[] {
+                0, 1, 2,
+                0, 2, 3,
+            };
+
+            uint vertices = Gl.GenBuffer();
+            Gl.BindBuffer(GLEnum.ArrayBuffer, vertices);
+            Gl.BufferData(GLEnum.ArrayBuffer, (ReadOnlySpan<float>)vertexArray.AsSpan(), GLEnum.StaticDraw);
+            // 0 is position
+            // 2 is normals
+            uint offsetPos = 0;
+            uint offsetNormals = offsetPos + 3 * sizeof(float);
+            uint vertexSize = offsetNormals + 3 * sizeof(float);
+            Gl.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, vertexSize, (void*)offsetPos);
+            Gl.EnableVertexAttribArray(0);
+            Gl.VertexAttribPointer(2, 3, VertexAttribPointerType.Float, true, vertexSize, (void*)offsetNormals);
+            Gl.EnableVertexAttribArray(2);
+            Gl.BindBuffer(GLEnum.ArrayBuffer, 0);
+
+            uint colors = Gl.GenBuffer();
+            Gl.BindBuffer(GLEnum.ArrayBuffer, colors);
+            Gl.BufferData(GLEnum.ArrayBuffer, (ReadOnlySpan<float>)colorArray.AsSpan(), GLEnum.StaticDraw);
+            // 1 is color
+            Gl.VertexAttribPointer(1, 4, VertexAttribPointerType.Float, false, 0, null);
+            Gl.EnableVertexAttribArray(1);
+            Gl.BindBuffer(GLEnum.ArrayBuffer, 0);
+
+            uint indices = Gl.GenBuffer();
+            Gl.BindBuffer(GLEnum.ElementArrayBuffer, indices);
+            Gl.BufferData(GLEnum.ElementArrayBuffer, (ReadOnlySpan<uint>)indexArray.AsSpan(), GLEnum.StaticDraw);
+            Gl.BindBuffer(GLEnum.ElementArrayBuffer, 0);
+
+            return new ModelObjectDescriptor() { Vao = vao, Vertices = vertices, Colors = colors, Indices = indices, IndexArrayLength = (uint)indexArray.Length, Gl = Gl };
+
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: dispose managed state (managed objects)
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override finalizer
+                // TODO: set large fields to null
+
+
+                // always unbound the vertex buffer first, so no halfway results are displayed by accident
+                Gl.DeleteBuffer(Vertices);
+                Gl.DeleteBuffer(Colors);
+                Gl.DeleteBuffer(Indices);
+                Gl.DeleteVertexArray(Vao);
+
+                disposedValue = true;
+            }
+        }
+
+        ~ModelObjectDescriptor()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: false);
+        }
+
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
