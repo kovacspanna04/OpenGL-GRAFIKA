a5116716b882ea91b3ba1dcc1a4431060645289e
kovacspanna04 <kovacspanna3@gmail.com>
2025-05-11 14:50:35 +0300

    javitva(szepitve+egyszerusitve)

--- Szeminarium4/Szeminarium1_24_03_05_2/ObjectResourceReader.cs
+++ Szeminarium4/Szeminarium1_24_03_05_2/ObjectResourceReader.cs
@@ -3,9 +3,10 @@ using Silk.NET.OpenGL;
 using System;
 using System.Collections.Generic;
 using System.Globalization;
-using System.IO;
 using System.Linq;
 using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Szeminarium1_24_03_05_2
 {
@@ -13,11 +14,9 @@ namespace Szeminarium1_24_03_05_2
     {
         public static unsafe GlObject CreateObjectFromResource(GL Gl, string resourceName)
         {
-            List<float[]> objVertices = new List<float[]>();
-            List<float[]> objNormals = new List<float[]>();     // normalvektorok
-            List<(int vIdx, int? vnIdx)> faceVertexData = new List<(int, int?)>();
-            List<int[]> faceIndices = new List<int[]>();
-            bool usesVertexNormals = false;     // jelzi, hogy van e normalvektor hasznalva
+            List<float[]> objVertices = new List<float[]>();        // v
+            List<float[]> objNormals = new List<float[]>();         // vn
+            List<(int v, int vn)[]> objFaces = new List<(int, int)[]>();         // f (pl. 1//1 2//2 3//3)
 
             string fullResourceName = "Szeminarium1_24_03_05_2.Resources." + resourceName;
             using (var objStream = typeof(ObjectResourceReader).Assembly.GetManifestResourceStream(fullResourceName))
@@ -27,91 +26,73 @@ namespace Szeminarium1_24_03_05_2
                 {
                     var line = objReader.ReadLine();
 
-                    if (string.IsNullOrWhiteSpace(line) || line.StartsWith("#"))        // ures sorokat es kommenteket kihagy
+                    if (string.IsNullOrWhiteSpace(line) || !line.Contains(' '))     // az ures sorokat is ugorja at
                         continue;
 
-                    var parts = line.Trim().Split(' ', StringSplitOptions.RemoveEmptyEntries);      // feldarabolja a sort szokozok menten
-                    if (parts.Length == 0)
-                        continue;
+                    var lineClassifier = line.Substring(0, line.IndexOf(' '));          // v, vn vagy f tipus
+                    var lineData = line.Substring(line.IndexOf(" ")).Trim().Split(' ');
 
-                    switch (parts[0])
+                    switch (lineClassifier)
                     {
-                        case "v":       // a v kezdetu sor koordinatat jelent, beolvassa a 3 koordinatat
-                            objVertices.Add(new float[]
-                            {
-                                float.Parse(parts[1], CultureInfo.InvariantCulture),
-                                float.Parse(parts[2], CultureInfo.InvariantCulture),
-                                float.Parse(parts[3], CultureInfo.InvariantCulture)
-                            });
+                        case "v":
+                            float[] vertex = new float[3];
+                            for (int i = 0; i < 3; i++)
+                                vertex[i] = float.Parse(lineData[i], CultureInfo.InvariantCulture);
+                            objVertices.Add(vertex);
                             break;
-
-                        case "vn":      // a vn a normalvektor, szinten 3 komponense van
-                            objNormals.Add(new float[]
-                            {
-                                float.Parse(parts[1], CultureInfo.InvariantCulture),
-                                float.Parse(parts[2], CultureInfo.InvariantCulture),
-                                float.Parse(parts[3], CultureInfo.InvariantCulture)
-                            });
+                        case "vn":
+                            float[] normal = new float[3];
+                            for (int i = 0; i < 3; i++)
+                                normal[i] = float.Parse(lineData[i], CultureInfo.InvariantCulture);
+                            objNormals.Add(normal);
                             break;
-
-                        case "f":       // a haromszog minden csucsahoz elmenti a pontot es a normalvektor indexet(ha van) csokkentve 1-el
-                            int[] face = new int[3];
+                        case "f":       // a haromszog 3 csucsa
+                            var face = new (int, int)[3];
                             for (int i = 0; i < 3; i++)
                             {
-                                var vertexParts = parts[i + 1].Split('/');
-                                int vIdx = int.Parse(vertexParts[0]) - 1;
-                                int? vnIdx = (vertexParts.Length >= 3 && !string.IsNullOrEmpty(vertexParts[2]))
-                                    ? int.Parse(vertexParts[2]) - 1
-                                    : null;
-
-                                faceVertexData.Add((vIdx, vnIdx));
-                                face[i] = vIdx;
-
-                                if (vnIdx.HasValue)
-                                    usesVertexNormals = true;
+                                var parts = lineData[i].Split('/');
+                                int vertexIndex = int.Parse(parts[0]) - 1;  // csucs index
+                                int normalIndex = parts.Length > 1 ? int.Parse(parts[2]) - 1 : -1;  // normal index
+                                face[i] = (vertexIndex, normalIndex);
                             }
-                            faceIndices.Add(face);
+                            objFaces.Add(face);
+                            break;
+                        default:
                             break;
                     }
+
                 }
             }
 
-
-            // lista minden vertexbol sajat strukturaval, ami tartalmazza a poziciot es a normalt
             List<ObjVertexTransformationData> vertexTransformations = new List<ObjVertexTransformationData>();
-            for (int i = 0; i < objVertices.Count; ++i)
+            for (int i = 0; i < objVertices.Count; i++)     // vegigmegyek az osszes csucsponton
             {
-                var coord = objVertices[i];
-                Vector3D<float> normal = Vector3D<float>.Zero;
+                var coords = objVertices[i];            // lekerem az aktualis csucs koordinatait
+                Vector3D<float> normal = Vector3D<float>.Zero;      // letrehozom a noralvektort, zero, mert lehet, hogy nincs
 
-                if (usesVertexNormals)
+                if (i < objNormals.Count)       // megnezem, hogy az akt. indexhez van e normalvektor
                 {
-                    var match = faceVertexData.FirstOrDefault(p => p.vIdx == i && p.vnIdx.HasValue);
-                    if (match.vnIdx.HasValue && match.vnIdx.Value < objNormals.Count)
-                    {
-                        var n = objNormals[match.vnIdx.Value];
-                        normal = new Vector3D<float>(n[0], n[1], n[2]);
-                    }
+                    var norm = objNormals[i];
+                    normal = new Vector3D<float>(norm[0], norm[1], norm[2]);        // ha van, atalakitom Vector3D tipusra
                 }
 
                 vertexTransformations.Add(new ObjVertexTransformationData(
-                    new Vector3D<float>(coord[0], coord[1], coord[2]),
-                    normal,
-                    1
+                    new Vector3D<float>(coords[0], coords[1], coords[2]),       // pozicio
+                    normal,                 // normalvektor amit fent beallitottam
+                    0           // extra adat
                 ));
             }
 
-            
-            // ha nem volt normalvektor, akkor kiszamolja a haromszogekbol az erteket vektorialis szorzattal
-            if (!usesVertexNormals)
-            {
-                foreach (var face in faceIndices)
-                {
-                    var a = vertexTransformations[face[0]];
-                    var b = vertexTransformations[face[1]];
-                    var c = vertexTransformations[face[2]];
 
-                    var normal = Vector3D.Normalize(Vector3D.Cross(b.Coordinates - a.Coordinates, c.Coordinates - a.Coordinates));
+            if (objNormals.Count == 0)      // ha nincs normalvektor, akkor kiszamolom a haromszogekbol
+            {
+                foreach (var face in objFaces)
+                {
+                    var a = vertexTransformations[face[0].v];
+                    var b = vertexTransformations[face[1].v];
+                    var c = vertexTransformations[face[2].v];
+
+                    var normal = Vector3D.Normalize(Vector3D.Cross(b.Coordinates - a.Coordinates, c.Coordinates - a.Coordinates)); // keresztszorzat
 
                     a.UpdateNormalWithContributionFromAFace(normal);
                     b.UpdateNormalWithContributionFromAFace(normal);
@@ -119,29 +100,30 @@ namespace Szeminarium1_24_03_05_2
                 }
             }
 
-            
+
             List<float> glVertices = new List<float>();
             List<float> glColors = new List<float>();
-            foreach (var vt in vertexTransformations)
+            foreach (var vertexTransformation in vertexTransformations)
             {
-                glVertices.Add(vt.Coordinates.X);
-                glVertices.Add(vt.Coordinates.Y);
-                glVertices.Add(vt.Coordinates.Z);
+                glVertices.Add(vertexTransformation.Coordinates.X);
+                glVertices.Add(vertexTransformation.Coordinates.Y);
+                glVertices.Add(vertexTransformation.Coordinates.Z);
 
-                glVertices.Add(vt.Normal.X);
-                glVertices.Add(vt.Normal.Y);
-                glVertices.Add(vt.Normal.Z);
+                glVertices.Add(vertexTransformation.Normal.X);
+                glVertices.Add(vertexTransformation.Normal.Y);
+                glVertices.Add(vertexTransformation.Normal.Z);
 
                 glColors.AddRange([1.0f, 0.0f, 0.0f, 1.0f]);
             }
 
             List<uint> glIndexArray = new List<uint>();
-            foreach (var face in faceIndices)
+            foreach (var face in objFaces)
             {
-                glIndexArray.Add((uint)face[0]);
-                glIndexArray.Add((uint)face[1]);
-                glIndexArray.Add((uint)face[2]);
+                glIndexArray.Add((uint)face[0].v);
+                glIndexArray.Add((uint)face[1].v);
+                glIndexArray.Add((uint)face[2].v);
             }
+
 
             uint vao = Gl.GenVertexArray();
             Gl.BindVertexArray(vao);
@@ -158,20 +140,26 @@ namespace Szeminarium1_24_03_05_2
             Gl.VertexAttribPointer(2, 3, VertexAttribPointerType.Float, true, vertexSize, (void*)offsetNormals);
             Gl.EnableVertexAttribArray(2);
 
+
             uint colors = Gl.GenBuffer();
             Gl.BindBuffer(BufferTargetARB.ArrayBuffer, colors);
             Gl.BufferData(BufferTargetARB.ArrayBuffer, (ReadOnlySpan<float>)glColors.ToArray().AsSpan(), BufferUsageARB.StaticDraw);
             Gl.VertexAttribPointer(1, 4, VertexAttribPointerType.Float, false, 0, null);
             Gl.EnableVertexAttribArray(1);
 
+
             uint indices = Gl.GenBuffer();
             Gl.BindBuffer(BufferTargetARB.ElementArrayBuffer, indices);
             Gl.BufferData(BufferTargetARB.ElementArrayBuffer, (ReadOnlySpan<uint>)glIndexArray.ToArray().AsSpan(), BufferUsageARB.StaticDraw);
 
+            // make sure to unbind array buffer
             Gl.BindBuffer(BufferTargetARB.ArrayBuffer, 0);
+
+            uint indexArrayLength = (uint)glIndexArray.Count;
+
             Gl.BindVertexArray(0);
 
-            return new GlObject(vao, vertices, colors, indices, (uint)glIndexArray.Count, Gl);
+            return new GlObject(vao, vertices, colors, indices, indexArrayLength, Gl);
         }
     }
 }
